f10c0b39ac43ae1fe7815b2889676b0f55b2f48c
Kaylie Lane <k.lane5612@gmail.com>
2022-04-24 17:28:52 -0400

    reset button

--- public/index.js
+++ public/index.js
@@ -2,6 +2,12 @@ async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    //add new button - to RESET counter
+    const resetButton = document.createElement('button');
+    resetButton.style.backgroundColor = 'red'
+    resetButton.style.color = 'white'
+    resetButton.textContent = 'Reset'
+    document.getElementsByTagName('div')[2].appendChild(resetButton);
 
     const response = await fetch('http://localhost:9001/counter');
 
@@ -12,7 +18,7 @@ async function main(){
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
-
+//each time increment function runs, server data is 'PATCHED'
         fetch('http://localhost:9001/counter', {
 
         method: 'PATCH',
@@ -28,6 +34,24 @@ async function main(){
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+//each time increment function runs, server data is 'PATCHED'
+        fetch('http://localhost:9001/counter', {
+
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                'value': countValue,
+            })
+        })
+        decrementValue = countValue;
+    }
+//reset function to 0, but does not preserve the page load value
+    function resetNow() {
+
+        countValue = 0;
+        countContainer.textContent=countValue;
         fetch('http://localhost:9001/counter', {
 
             method: 'PATCH',
@@ -42,6 +66,8 @@ async function main(){
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    //Reset function event listener
+    resetButton.addEventListener('click', resetNow);
     countContainer.textContent = countValue;
 }
 main()
